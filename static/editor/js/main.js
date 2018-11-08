96aec48a3e346896610c57dcc89111e55c83a6a6
nice <244039784@qq.com>
2018-11-08 10:40:32 +0800

    文本初始化

--- static/editor/js/main.js
+++ static/editor/js/main.js
@@ -97,7 +97,7 @@ var bindBold = function() {
   })
 }
 
-var bindItalic = function(){
+var bindItalic = function() {
   var italic = $('.glyphicon-italic')
   italic.click(function() {
     var textarea = document.querySelector('.editor')
@@ -120,7 +120,7 @@ var bindItalic = function(){
   })
 }
 
-var bindOl = function(){
+var bindOl = function() {
   var ol = $('.glyphicon-sort-by-order')
   ol.click(function() {
     var textarea = document.querySelector('.editor')
@@ -143,7 +143,7 @@ var bindOl = function(){
   })
 }
 
-var bindUl = function(){
+var bindUl = function() {
   var ul = $('.glyphicon-list')
   ul.click(function() {
     var textarea = document.querySelector('.editor')
@@ -165,9 +165,9 @@ var bindUl = function(){
     $('.rightbox').html(html);
   })
 }
-var bindHead = function(){
+var bindHead = function() {
   var head = $('.glyphicon-header')
-  head.click(function(){
+  head.click(function() {
     var textarea = document.querySelector('.editor')
     var position = getCursorPosition(textarea)
     var converter = new showdown.Converter();
@@ -186,9 +186,9 @@ var bindHead = function(){
   })
 }
 
-var bindLine = function(){
+var bindLine = function() {
   var line = $('.glyphicon-minus')
-  line.click(function(){
+  line.click(function() {
     var textarea = document.querySelector('.editor')
     var position = getCursorPosition(textarea)
     var converter = new showdown.Converter();
@@ -202,9 +202,9 @@ var bindLine = function(){
   })
 }
 
-var bindTheme = function(){
+var bindTheme = function() {
   var theme = $('.glyphicon-adjust')
-  theme.click(function(){
+  theme.click(function() {
     $('.editor').toggleClass('dark')
     $('.rightbox').toggleClass('dark')
     $('.leftbox').toggleClass('dark')
@@ -225,6 +225,51 @@ var main = function() {
   bindHead()
   bindLine()
   bindTheme()
+  $('.editor').ready(function() {
+    text = `#本文引用自[Cmd Markdown ](https://www.zybuluo.com/mdeditor)仅作为测试用
+#侵删联系作者：244039784@qq.com
+
+-----
+
+## 欢迎使用 Cmd Markdown 编辑阅读器
+
+------
+
+我们理解您需要更便捷更高效的工具记录思想，整理笔记、知识，并将其中承载的价值传播给他人，**Cmd Markdown** 是我们给出的答案 —— 我们为记录思想和分享知识提供更专业的工具。 您可以使用 Cmd Markdown：
+
+> * 整理知识，学习笔记
+> * 发布日记，杂文，所见所想
+> * 撰写发布技术文稿（代码支持）
+> * 撰写发布学术论文（LaTeX 公式支持）
+
+![cmd-markdown-logo](https://www.zybuluo.com/static/img/logo.png)
+
+除了您现在看到的这个 Cmd Markdown 在线版本，您还可以前往以下网址下载：
+
+### [Windows/Mac/Linux 全平台客户端](https://www.zybuluo.com/cmd/)
+
+> 请保留此份 Cmd Markdown 的欢迎稿兼使用说明，如需撰写新稿件，点击顶部工具栏右侧的 <i class="icon-file"></i> **新文稿** 或者使用快捷键 'Ctrl+Alt+N'。
+
+------
+
+## 什么是 Markdown
+
+Markdown 是一种方便记忆、书写的纯文本标记语言，用户可以使用这些标记符号以最小的输入代价生成极富表现力的文档：譬如您正在阅读的这份文档。它使用简单的符号标记不同的标题，分割不同的段落，**粗体** 或者 *斜体* 某些文字，更棒的是，它还可以
+
+### 1. 制作一份待办事宜 [Todo 列表](https://www.zybuluo.com/mdeditor?url=https://www.zybuluo.com/static/editor/md-help.markdown#13-待办事宜-todo-列表)
+
+- [ ] 支持以 PDF 格式导出文稿
+- [ ] 改进 Cmd 渲染算法，使用局部渲染技术提高渲染效率
+- [x] 新增 Todo 列表功能
+- [x] 修复 LaTex 公式渲染问题
+- [x] 新增 LaTex 公式编号功能`
+    $('.editor').val(text)
+    var converter = new showdown.Converter()
+    var size = $('.editor').css('font-size')
+    var html = converter.makeHtml(text)
+    $('.rightbox').html(html)
+    $('.rightbox').css('font-size', size)
+  })
 }
 
 main()
